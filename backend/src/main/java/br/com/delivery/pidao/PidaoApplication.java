01ab76546806ec43ab19c13799d424e2272d88b2
Eriky <erikyryang@gmail.com>
2022-09-14 21:38:15 -0300

    start backend

---
+++ backend/src/main/java/br/com/delivery/pidao/PidaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.delivery.pidao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/delivery/pidao/PidaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivery.pidao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
