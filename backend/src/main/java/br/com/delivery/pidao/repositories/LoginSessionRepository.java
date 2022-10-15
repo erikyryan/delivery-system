ed2c80c83cd702973337dadfd3ba931baa7e7aef
Eriky Ryan Gonçalves Leite <eriky.leite@portaldedocumentos.com.br>
2022-10-14 21:31:01 -0300

    first step session login

---
+++ backend/src/main/java/br/com/delivery/pidao/repositories/LoginSessionRepository.java
@@ -0,0 +1,7 @@
+package br.com.delivery.pidao.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LoginSessionRepository {
+}
---
+++ backend/src/main/java/br/com/delivery/pidao/services/SessionService.java
@@ -0,0 +1,56 @@
+package br.com.delivery.pidao.services;
+
+public class SessionService {
+//    private LoginSessionRepository loginSessionRepository;
+//
+//    private UserService userService;
+//
+//    public LoginSessionService(@Lazy LoginSessionRepository loginSessionRepository, UserService userService){
+//        this.loginSessionRepository = loginSessionRepository;
+//        this.userService = userService;
+//    }
+//
+//    public void validateToken(String token) {
+//        LoginSession session = this.findSessionByToken(token);
+//        Date expirationDate = session.getExpirationDate();
+//        if (new Date().after(expirationDate)) {
+//            throw new RuntimeException("Sessão expirou.");
+//        }
+//    }
+//
+//    public String generateSession(User userS) {
+//        Optional<LoginSession> session = loginSessionRepository.findByUserIdentifierAndLogoutDateNull(userS.getUserIdentifier());
+//        if (session.isPresent()) {
+//            session.get().finish();
+//        }
+//
+//        LoginSession userSession = new LoginSession(userS);
+//        loginSessionRepository.save(userSession);
+//
+//        return userSession.getToken();
+//    }
+//
+//    public LoginSession findSessionByToken(String token) {
+//        Optional<LoginSession> session = loginSessionRepository.findByToken(token);
+//        if (session.isPresent()) {
+//            return session.get();
+//        }else {
+//            throw new RuntimeException("não existe essa sessão");
+//        }
+//    }
+//
+//    public User findUser(String token) {
+//        LoginSession session = this.findSessionByToken(token);
+//        User user = userService.findByIdentifier(session.getUserIdentifier());
+//        return user;
+//    }
+//
+//    public LoginSession logout(String token) {
+//        LoginSession session = this.findSessionByToken(token);
+//        session.finish();
+//
+//        loginSessionRepository.save(session);
+//
+//        return session;
+//    }
+}
