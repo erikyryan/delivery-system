d1b949f95537ca25f08105d302e72033bf2ca2af
Eriky Ryan Gonçalves Leite <erikyryang@gmail.com>
2022-11-05 18:17:21 -0300

    test layer init

--- backend/src/main/java/br/com/delivery/pidao/entities/dto/MenuDTO.java
+++ backend/src/main/java/br/com/delivery/pidao/entities/dto/MenuDTO.java
@@ -1,7 +1,13 @@
 package br.com.delivery.pidao.entities.dto;
 
 import br.com.delivery.pidao.entities.Menu;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class MenuDTO {
 
     private String restaurantIdentifier;
